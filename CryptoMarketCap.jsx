67235b3638f84983b30dbf1c04c3c1e0bea065e2
Manyan Chan <ss121317@connect.hku.hk>
2021-05-27 18:24:34 +0800

    update script

--- CryptoMarketCap.jsx
+++ CryptoMarketCap.jsx
@@ -8,7 +8,6 @@ LICENSE file in the root directory of this source tree.
 
 // update every 5 mins
 const update_interval = 1000 * 60 * 5;
-
 export const className = `
 	top: 150px;
 	right: 20px;
@@ -18,22 +17,24 @@ export const className = `
 	background-color: rgba(0,0,0,0.3);
 	text-align: right;
 	-webkit-font-smoothing: antialiased;
-
-	span {
-		font-size: 16px;
-		font-weight: normal;
+	p {
+		margin: 0;
 	}
-
 	div {
 		width: auto;
 		padding: 20px;
 	}
-
-	p {
+	.Number {
 		font-size: 2em;
 		font-weight: 200;
 		margin-top: 5px;
 		margin-bottom:0;
+	}
+	.Positive{
+		color: #1dcc5a;
+	}
+	.Negative{
+		color: #cf1111;
 	}
 }`;
 
@@ -60,23 +61,20 @@ const execute = (action, interval) => {
 };
 
 export const render = ({ cap, change }) => {
-	function numberWithCommas(x) {
+	const numberWithCommas = (x) => {
 		return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
-	}
+	};
+
 	const num = `$ ${numberWithCommas(Math.round(cap))}`;
 	console.log(change);
 
 	return (
-		<div className='cryptoMarketCap'>
-			<span>Crypto Market Cap</span>
-			<p>{num}</p>
-			<span
-				style={{
-					color: change > 0 ? '#1dcc5a' : '#cf1111',
-					fontWeight: 'normal',
-				}}>
+		<div className='CryptoMarketCap'>
+			<p>Crypto Market Cap</p>
+			<p className='Number'>{num}</p>
+			<p className={change > 0 ? 'Positive' : 'Negative'}>
 				{Number.parseFloat(change).toPrecision(3) + '%'}
-			</span>
+			</p>
 		</div>
 	);
 };
